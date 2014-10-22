423bca95dcdf8855768813226203a6c95a7ded18
Chris Dawson <cdawson@ebay.com>
2014-10-22 16:57:50 -0700

    Added thor cli

--- lib/reproxy.rb
+++ lib/reproxy.rb
@@ -1,5 +1,39 @@
 require "reproxy/version"
+require 'thor'
 
 module Reproxy
-  # Your code goes here...
+  class Proxy < Thor
+
+    desc "record", "Record a proxy session"
+    option :name, required: true
+    def record
+    end
+
+    desc "replay", "Replay a proxy session"
+    option :name, required: true
+    def replay
+    end
+
+    desc "edit", "Edit a proxy session file"
+    option :name, required: true
+    option :revision, required: true
+    def edit
+    end
+
+    desc "list", "List all proxy sessions"
+    option :filter
+    def list
+    end
+
+    desc "info", "Print information about a proxy session"
+    option :name, required: true
+    def info
+    end
+
+    desc "list-files", "List proxy session files for a session"
+    option :name, required: true
+    def list_files
+    end
+
+  end
 end
